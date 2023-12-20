eaf705d8eeb238b3560a32d8b985561817ea7697
Andrejioo <proo0984@gmail.com>
2023-12-20 23:25:49 +0300

    ы

---
+++ src/bsu/rfct/lab2/vyalkin/var9/Main.java
@@ -0,0 +1,14 @@
+package bsu.rfct.lab2.vyalkin.var9;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
+}
